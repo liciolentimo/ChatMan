8373ee5b627222f07a0d94da5d5033a662b6b0d2
Licio Lentimo <lentimo@gmail.com>
2018-05-29 15:31:29 +0300

    Added register activity

---
+++ app/src/main/java/com/lentimosystems/chatman/RegisterActivity.java
@@ -0,0 +1,69 @@
+package com.lentimosystems.chatman;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+    private TextInputLayout mName;
+    private  TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateBtn;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mName = (TextInputLayout)findViewById(R.id.reg_display_name);
+        mEmail = (TextInputLayout)findViewById(R.id.reg_email);
+        mPassword = (TextInputLayout)findViewById(R.id.txtPassword);
+        mCreateBtn = (Button)findViewById(R.id.btn_create_account);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String display_name = mName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+
+                register_user(display_name,email,password);
+            }
+        });
+
+    }
+
+    private void register_user(String display_name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
+                            startActivity(mainIntent);
+                            finish();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+}
--- app/src/main/java/com/lentimosystems/chatman/StartActivity.java
+++ app/src/main/java/com/lentimosystems/chatman/StartActivity.java
@@ -1,13 +1,27 @@
 package com.lentimosystems.chatman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    private Button mRegBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        mRegBtn = (Button)findViewById(R.id.btn_register);
+
+        mRegBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg_intent = new Intent(StartActivity.this,RegisterActivity.class);
+                startActivity(reg_intent);
+            }
+        });
     }
 }
